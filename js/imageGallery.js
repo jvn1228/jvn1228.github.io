d3889533d95e87b1de88ecc95d9087a495ef29ac
Jonathan Nguyen <jonathannguyen@Jonathans-MacBook-Pro-5.local>
2018-02-19 17:31:00 -0600

    Added comments, now Amazon S3 ready

--- js/imageGallery.js
+++ js/imageGallery.js
@@ -1,7 +1,23 @@
+/* imageGallery.js
+Creates a mosaic-style image gallery using j-query. Amazon S3 ready.
+Image thumbnails can be linked to larger images, which open in a modal
+window.
+
+Helper files: imageModal.js, s3GetImages.js
+
+Input parameters:
+-images: array of image thumbnails
+-imgsFull: array of full-size image files
+-bucket: Amazon S3 bucket, must be public readable
+*/
+
+/*
 var images = ["imgThumbnail-1.jpg","imgThumbnail-2.jpg","imgThumbnail-3.jpg","imgThumbnail-4.jpg","imgThumbnail-5.jpg","imgThumbnail-6.jpg","imgThumbnail-7.jpg","imgThumbnail-8.jpg","imgThumbnail-9.jpg","imgThumbnail-10.jpg","imgThumbnail-11.jpg","imgThumbnail-12.jpg","imgThumbnail-13.jpg","imgThumbnail-14.jpg"];
 var imgsFull = ["imgFull-1.jpg","imgFull-2.jpg","imgFull-3.jpg","imgFull-4.jpg","imgFull-5.jpg","imgFull-6.jpg","imgFull-7.jpg","imgFull-8.jpg","imgFull-9.jpg","imgFull-10.jpg","imgFull-11.jpg","imgFull-12.jpg","imgFull-13.jpg","imgFull-14.jpg",]
 var imagePath = "media/thumbnails/";
 var fullImagePath = "media/photos/";
+*/
+
 var columns = 4;
 var imageGrid = [];
 var imageWidth = 500;
@@ -10,13 +26,24 @@ var verticalOffset = 70;
 var horizontalOffset = 50;
 var imageBorder = 2;
 var verPos = verticalOffset;
+
 var i = 0;
 
 
-$(document).ready(function(){
+function imageGallery(images,imgsFull,bucket){
 	
-setTimeout(function(){buildGallery();},100);
+	var imagePath = "https://s3.us-east-2.amazonaws.com/" + bucket + "/";
+	var fullImagePath = "https://s3.us-east-2.amazonaws.com/" + bucket + "/";
+
+	//Images fade onto the page
+	setTimeout(function(){buildGallery();},100);
 			
+	/*getNewImg(callback)
+	Called by buildGallery() to find image dimensions, required to properly
+	stack the images in a mosaic style. Since x.onload is asynchronous, the
+	callback function is executed after the image is loaded; otherwise,
+	size is reported as 0 unless the page is refreshed (cached)
+	*/
 	function getNewImg(callback){
 		var imageObj = new Image();
 		imageObj.name = imagePath+images[i];
@@ -30,7 +57,10 @@ setTimeout(function(){buildGallery();},100);
 		return
 	}
 	
-	
+	/* buildGallery()
+	Main helper function which recursively adds styled html img elements
+	to the page at calculated locations.
+	*/
 	function buildGallery(){
 		
 		if (i < images.length) {
@@ -38,7 +68,9 @@ setTimeout(function(){buildGallery();},100);
 			console.log('Current index: '+i);
 			
 			getNewImg(function(imgWidth, imgHeight){
-			
+				
+				//Callback function begins here, which does the page rendering 
+				//work and ratio calculations
 				console.log("Within callback function size: "+imgWidth+"x"+imgHeight);
 				console.log("Actual ratio: "+(imgHeight/imgWidth));
 				
@@ -61,12 +93,17 @@ setTimeout(function(){buildGallery();},100);
 				$(".gallery").append("<div class=\""+imageClass+"\"><img src=\""+imagePath+image+"\" onload=\"$(this).fadeIn(2000);\" onclick=\"imageModal("+fullImage+")\"></div>");
 				$("."+imageClass+" img").css({"width":imageWidth,"height":"auto","border":imageBorder+"px solid black","z-index":i+100});
 				$("."+imageClass).css({"display":"inline-block","padding":"5px","position":"absolute","left":horPos+"px","top":verPos+"px","cursor":"pointer"});
-			
+				
+				//Recursive call, advance the image list, so only one image
+				//placed at a time
 				return setTimeout(function(){i++;buildGallery();},100);
 
 			});
 			
-			
+			//If i++ is placed outside the callback function,
+			//We have to cache the images but on second refresh, multiple
+			//images are rendered at once and out of order,
+			//as the onload is completed.
 			
 		}
 		else {
@@ -74,4 +111,4 @@ setTimeout(function(){buildGallery();},100);
 		}
 			
 	}
-});
+}
