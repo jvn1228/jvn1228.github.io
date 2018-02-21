a9e1f7e0e48211a4d121783e5f1b58195d5c0493
Jonathan Nguyen <jonathannguyen@Jonathans-MacBook-Pro-5.local>
2018-02-21 13:37:58 -0600

    Gallery helper

---
+++ js/s3GetImages.js
@@ -0,0 +1,27 @@
+AWS.config.update({accessKeyId: 'AKIAIG7GOO2HSNZUCW4A', secretAccessKey: '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'});
+AWS.config.region = "us-east-2";
+var params = {
+	Bucket: 'jvn1228-image-data',
+	Delimiter: '/',
+	EncodingType: 'url'
+}
+var bucket = new AWS.S3({params});
+
+$(document).ready(function(){
+
+	bucket.listObjects(function (err, data) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			var dataLength = data.Contents.length;
+			var images = [];
+			for(var i = 0; i < dataLength; i++){
+				images.push(data.Contents[i].Key);
+			}	
+			imageGallery(images, images, params.Bucket);
+			console.log(images);
+		}
+	})
+	
+})
