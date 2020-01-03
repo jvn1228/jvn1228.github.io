492d4b030b13c3e2df57cc7f2f3f694b33a41f20
jvn1228 <jvn1228@gmail.com>
2020-01-03 16:16:45 -0600

    Complete overhaul of website. home page added

--- js/add.js
+++
@@ -1,7 +0,0 @@
-function add(){
-	const num1 = Number(document.getElementsByName("numDisp1")[0].value);
-	const num2 = Number(document.getElementsByName("numDisp2")[0].value);
-	const ans = num1 + num2;
-	console.log(ans);
-	document.getElementsByName("numDisp1")[0].value = ans;
-}
